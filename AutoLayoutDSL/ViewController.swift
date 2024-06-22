9878d8d82a203a9629a9382471a74e2c8d050c24
Oleh Yeroshkin <oyeroshkin30@gmail.com>
2024-06-23 00:34:24 +0300

    Project created. Storyboard removed.

---
+++ AutoLayoutDSL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutDSL
+//
+//  Created by Oleh Yeroshkin on 23.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
